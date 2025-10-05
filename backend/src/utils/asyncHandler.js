aef1605339b61ad6ec38f0b673c5755b711785ef
Jayraj Parmar <jayrajsinhp340@gmail.com>
2025-10-05 10:58:58 +0530

    Remove frontend React application files and configuration

--- backend/src/utils/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-const asyncHandler = (fn) => async (req, res, next) => {
-  try {
-    fn(req, res, next);
-  } catch (error) {
-    res.status(error.status || 500).json({ msg: error.message });
-  }
-};
-
-export { asyncHandler };
